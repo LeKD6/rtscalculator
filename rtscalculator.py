21905f704f6ab284269b14374f3d6dd35f53f540
LeKD6 <144394206+LeKD6@users.noreply.github.com>
2023-10-31 14:08:53 -0700

    Update rtscalculator.py

--- rtscalculator.py
+++ rtscalculator.py
@@ -2,10 +2,15 @@ import streamlit as st
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
+import random
+import time
+
+time.sleep(random.uniform(1.5, 4.5))
 
 def fetch_data(year, season_type):
     player_stats_url = f"https://www.basketball-reference.com/{season_type}/NBA_{year}_per_game.html"
-    response = requests.get(player_stats_url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(player_stats_url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find(name='table', id='per_game_stats')
     df = pd.read_html(str(table))[0]
@@ -125,18 +130,7 @@ if start_year and end_year and season_display:
     unique_teams = df_player_stats['Tm'].dropna().unique().tolist()
     unique_players = df_player_stats['Player'].dropna().unique().tolist()
 
-    st.header("To Compare Players")
-    player1 = st.selectbox("Select Player 1:", ['Select'] + unique_players)
-    player2 = st.selectbox("Select Player 2:", ['Select'] + unique_players)
 
-    if player1 != 'Select' and player2 != 'Select':
-        comparison_df = pd.concat([df_player_stats[df_player_stats['Player'] == player1], 
-                                   df_player_stats[df_player_stats['Player'] == player2]])
-        comparison_df = comparison_df.round({'PTS': 1, 'TS%': 1, 'rTS%': 1, '3P%': 1, 'r3P%': 1, 'rFT%': 1, 'AST:TOV': 1, 'rAST:TOV': 1})
-        st.table(comparison_df[['Player', 'Year', 'Tm', 'G', 'FGA', 'PTS', 'AST', 'TRB', 'TS%', 'rTS%', '3PA', '3P%', 'r3P%', 'FTA', 'FT%', 'rFT%', 'AST:TOV', 'rAST:TOV']])
-
-
-    st.header("For Individual or Teams Use Either of the Below Filters")
     team = st.selectbox("Select Team:", ['Select'] + unique_teams)
     player = st.selectbox("Select Player:", ['Select'] + unique_players)
     mp = st.slider("Select Minimum MP:", min_value=0, max_value=48)
