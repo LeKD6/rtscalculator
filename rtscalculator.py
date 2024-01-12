77404c0a60d217b1785303664d67704d71fead36
LeKD6 <144394206+LeKD6@users.noreply.github.com>
2024-01-12 10:28:50 -0700

    Update rtscalculator.py

--- rtscalculator.py
+++ rtscalculator.py
@@ -85,8 +85,8 @@ def fetch_data_multi_years(start_year, end_year, season_type):
 
         # Calculate league-wide statistics for this specific year
         df['TS_league'] = TS_league
-        df['TPP'] * 100 = league_avg_3P 
-        df['FTP'] * 100 = league_avg_FT 
+        df['league_avg_3P'] = TPP * 100
+        df['league_avg_FT'] = FTP * 100
 
         # Calculate player metrics for this specific year
         df['TSA'] = df['FGA'] + 0.44 * df['FTA']
